42a7668553bf820589a0f6ce5fa07de84ac7a065
gem update rails <yogiagustiyahya@gmail.com>
2015-03-25 16:53:04 +0700

    adere

---
+++ proyek/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Proyek::Application.config.secret_token = 'ada80b21a9b75a6b7de63e3cd01b1d240e39ee4b7310596838f81f11814f84eaff02d6bc86b5cd1a98dd15c389e1a1692bd286c21ee852b7727219063f86d403'
